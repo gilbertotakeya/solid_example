8fb52334df39a59e06d5d2e68af665b9ead64eaa
gilbertotakeya <39373784+gilbertotakeya@users.noreply.github.com>
2023-01-24 16:15:08 -0300

    Add files via upload

---
+++ L.php
@@ -0,0 +1,28 @@
+<?php
+
+class A 
+{
+    public function getNome()
+    {
+        echo 'Meu nome é A';
+    }
+}
+
+class B extends A 
+{ 
+    public function getNome()
+    {
+        echo 'Meu nome é B';
+    }
+}
+
+$objeto1 = new A;
+$objeto2 = new B;
+
+function imprimeNome(A $objeto)
+{
+    return $objeto->getNome();
+}
+
+imprimeNome($objeto1); // Meu nome é A
+imprimeNome($objeto2); // Meu nome é B
